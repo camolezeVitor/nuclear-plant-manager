6fee31e1ab3e5da5763654d3767050dedd52951e
VitorHugoCD <vitordelantonia@gmail.com>
2024-11-14 11:44:39 -0300

    angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
 import { configurePrimengTheme } from '../configs/theme.config';
 import { ButtonModule } from "primeng/button";
@@ -9,13 +8,10 @@ import { ButtonModule } from "primeng/button";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
   template: `
-  <router-outlet />
   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et numquam veniam distinctio voluptatibus blanditiis quis nesciunt error nostrum hic officiis at iste ipsa, pariatur voluptatum! Iste consectetur cupiditate beatae mollitia.</p>
-  <p-button label="carai lek :)" />
   `,
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   config: PrimeNGConfig = inject(PrimeNGConfig);
