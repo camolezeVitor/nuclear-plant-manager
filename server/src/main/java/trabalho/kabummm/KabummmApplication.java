13c715a74fca01174a2f71e8b773c865350751cc
uryel <uryeljodelucca18@gmail.com>
2024-11-18 12:31:57 -0300

    feat: realizar o commit inicial da aplicação.

---
+++ server/src/main/java/trabalho/kabummm/KabummmApplication.java
@@ -0,0 +1,13 @@
+package trabalho.kabummm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KabummmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KabummmApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/trabalho/kabummm/KabummmApplicationTests.java
@@ -0,0 +1,13 @@
+package trabalho.kabummm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KabummmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
