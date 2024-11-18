a0d80a5ce0fe346e8a433d58ae10072f563eacf8
uryel <uryeljodelucca18@gmail.com>
2024-11-18 19:42:30 -0300

    feat: implementar a classe de corsFilterglobal

---
+++ server/src/main/java/trabalho/kabummm/config/Security/CorsFilterGlobal.java
@@ -0,0 +1,22 @@
+package trabalho.kabummm.config.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsFilterGlobal {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter (source);
+    }
+}
