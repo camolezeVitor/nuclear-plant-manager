f88ea857cd327dac758a6ee96c0eab840393c125
uryel <uryeljodelucca18@gmail.com>
2024-11-18 22:03:44 -0300

    feat: implementar a entidade de TipoSetor com seu repositório.

---
+++ server/src/main/java/trabalho/kabummm/entity/TipoSetorEntity.java
@@ -0,0 +1,26 @@
+package trabalho.kabummm.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tipo_setor", schema = "public")
+public class TipoSetorEntity {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "descricao")
+    private String descricao;
+}
